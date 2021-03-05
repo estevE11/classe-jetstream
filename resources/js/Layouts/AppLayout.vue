2286537bd4c309144a30f579f9024100a763def8
Roger Esteve <rogeresteve30@gmail.com>
2021-03-05 15:11:16 +0100

    Task Model and Migrations

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -20,6 +20,9 @@
                                 <jet-nav-link :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </jet-nav-link>
+                                <jet-nav-link :href="route('task.show')" :active="route().current('task.show')">
+                                    Tasks
+                                </jet-nav-link>
                             </div>
                         </div>
 
